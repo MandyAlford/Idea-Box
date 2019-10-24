6347ec78aa95518ec936a4aac5ed9a7b4a2b9894
Mandy Alford <mandyalford@gmail.com>
2019-10-24 07:36:25 -0600

    Update drop down menu function and rename variables

--- main.js
+++ main.js
@@ -2,15 +2,19 @@ var textInput = document.querySelector("#title-input");
 var cardTitle = document.querySelector("h2");
 var bodyInput = document.querySelector("body");
 var cardText = document.querySelector(".card-text");
-
-function (){
-  dropdownMenu.addEventListener("click", function(){
-
-  })
-}
+var expandedMenu = document.querySelector(".expanded-menu");
+// var asideExpanded = document.querySlector("aside");
 
 var dropdownMenu = document.querySelector(".dropdown-menu");
-var
+dropdownMenu.addEventListener("click", function(){
+  var aside = document.getElementsByClassName("starred")[0];
+  if (aside.style.display === "block") {
+   aside.style.display = "none";
+ } else {
+   aside.style.display = "block";
+ }
+  // asideExpanded.classList.toggle("expanded-menu");
+})
 //when click on dropdown menu, the menu pops up. an x replaces the nav
 //menu and only the h1, .starred, and #show are visible.
 //a gradient appears over the rest of the page
