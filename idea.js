054efd47ea90f889f0e5133d45b7c191f3c2108d
Virginia Ladd <virginialadd2@gmail.com>
2019-10-24 15:17:46 -0600

    Move everything off the idea.js except the class

--- idea.js
+++ idea.js
@@ -1,4 +1,3 @@
-
 class Idea {
   constructor(id) {
     this.id = id;
@@ -16,21 +15,3 @@ class Idea {
   updateIdea() {
   }
 }
-//Generate Random Number ID
-var outputAlph = "";
-var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var length = alphabet.length/9;
-var outputNumA = (Math.ceil(Math.random() * 1000));
-var outputNumB = (Math.ceil(Math.random() * 1000));
-
-  for ( var i = 0; i < length; i++ ) {
-      outputAlph += alphabet.charAt(Math.floor(Math.random() * length));
-   }
-
-console.log(outputAlph)
-console.log(outputNumA)
-console.log(outputNumB)
-
-var ID = `${outputAlph}-${outputNumA}-${outputNumB}`
-
-console.log(ID);
--- main.js
+++ main.js
@@ -23,3 +23,22 @@ function cardData(event) {
     </footer>
 </div>`;
 };
+
+//Generate Random Number ID
+var outputAlph = "";
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var length = alphabet.length/9;
+var outputNumA = (Math.ceil(Math.random() * 1000));
+var outputNumB = (Math.ceil(Math.random() * 1000));
+
+  for ( var i = 0; i < length; i++ ) {
+      outputAlph += alphabet.charAt(Math.floor(Math.random() * length));
+   }
+
+console.log(outputAlph)
+console.log(outputNumA)
+console.log(outputNumB)
+
+var ID = `${outputAlph}-${outputNumA}-${outputNumB}`
+
+console.log(ID);
