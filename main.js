d7a832741c9712d0a8eb8caa45d53b7588d01292
Melissa Steele <melissa.steele09@gmail.com>
2019-10-24 13:48:40 -0600

    Add form functionality

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-var textInput = document.querySelector("#title-input");
-var cardTitle = document.querySelector("h2");
-var bodyInput = document.querySelector("body");
-var cardText = document.querySelector(".card-text");
+var cardSection = document.querySelector(".bottom-section");
+var submitForm = document.querySelector("#save-btn");
 
-function (){
+submitForm.addEventListener("click", cardData);
 
-}
+function cardData(event) {
+  var cardTitleText = document.getElementById("title-input");
+  var cardBodyText = document.getElementById("body-input")
+  event.preventDefault();
+  cardSection.innerHTML += `
+    <div class="single-card">
+      <header class="header-icons">
+        <img src="./assets/star-active.svg">
+        <img  src="./assets/delete.svg">
+      </header>
+      <section class="card-body">
+        <h2>${cardTitleText.value}</h2>
+        <p class="card-text">${cardBodyText.value}</p>
+      </section>
+    <footer>
+      <img src="./assets/comment.svg" alt="comment icon">
+      <p>Comment</p>
+    </footer>
+</div>`;
+};
