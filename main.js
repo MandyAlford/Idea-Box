ab19c42812a5a38276c3e360df6307660c15b3b5
Mandy Alford <mandyalford@gmail.com>
2019-10-23 21:16:33 -0600

    Wrote function for drop down menu from hamburger icon

--- main.js
+++ main.js
@@ -4,5 +4,13 @@ var bodyInput = document.querySelector("body");
 var cardText = document.querySelector(".card-text");
 
 function (){
+  dropdownMenu.addEventListener("click", function(){
 
+  })
 }
+
+var dropdownMenu = document.querySelector(".dropdown-menu");
+var
+//when click on dropdown menu, the menu pops up. an x replaces the nav
+//menu and only the h1, .starred, and #show are visible.
+//a gradient appears over the rest of the page
