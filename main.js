becfd341775dbcaf38689bed1bbec306917cbf6a
Virginia Ladd <virginialadd2@gmail.com>
2019-10-28 16:30:41 -0600

    Add delete functionality on card

--- main.js
+++ main.js
@@ -1,47 +1,53 @@
+// ----------- Global Variables ------------
 var cardTitle = document.querySelector("h2");
 var expandedMenu = document.querySelector(".expanded-menu");
 var cardSection = document.querySelector(".bottom-section");
 var submitForm = document.querySelector("#save-btn");
+var deleteBtn = document.querySelector(".delete-btn");
+var faveBtn = document.querySelector("#fave-btn");
 // var asideExpanded = document.querySlector("aside");
+var dropDownMenu = document.querySelector(".dropdown-menu");
 
-var dropdownMenu = document.querySelector(".dropdown-menu");
-dropdownMenu.addEventListener("click", function(){
+
+// ----------- Event Listeners ------------
+submitForm.addEventListener("click", cardData);
+cardSection.addEventListener("click", deleteCard);
+cardSection.addEventListener("click", favoriteCard);
+
+dropDownMenu.addEventListener("click", function(){
   var aside = document.querySelector("#aside-menu");
-  console.log("works");
    aside.classList.toggle("starred-inactive");
    aside.classList.toggle("starred-media-active");
 });
-
-
-
-//when click on dropdown menu, the menu pops up. an x replaces the nav
-//menu and only the h1, .starred, and #show are visible.
-//a gradient appears over the rest of the page
-
-
-submitForm.addEventListener("click", cardData);
 
 function cardData(event) {
   var cardTitleText = document.getElementById("title-input");
   var cardBodyText = document.getElementById("body-input")
   event.preventDefault();
   cardSection.innerHTML += `
-    <div class="single-card">
+    <div class="single-card" "delete">
       <header class="header-icons">
-        <img src="./assets/star-active.svg">
-        <img  src="./assets/delete.svg">
+        <img class="fave-inactive" id="fave-btn" alt="favorite icon">
+        <img class="delete-btn" src="./assets/delete.svg" alt="delete icon">
       </header>
       <section class="card-body">
         <h2>${cardTitleText.value}</h2>
         <p class="card-text">${cardBodyText.value}</p>
       </section>
-    <footer>
-      <img src="./assets/comment.svg" alt="comment icon">
-      <p>Comment</p>
-    </footer>
-</div>`;
+      <footer>
+        <img src="./assets/comment.svg" alt="comment icon">
+        <p>Comment</p>
+      </footer>
+    </div>`;
 document.querySelector("form").reset();
 };
+
+function deleteCard(event) {
+  if(event.target.classList.contains("delete-btn")) {
+    event.target.parentNode.parentNode.remove();
+  }
+}
+
 
 // ----- Generate Random Number ID -----
 var outputAlph = "";
@@ -53,10 +59,6 @@ var outputNumB = (Math.ceil(Math.random() * 1000));
   for ( var i = 0; i < length; i++ ) {
       outputAlph += alphabet.charAt(Math.floor(Math.random() * length));
    }
-
-console.log(outputAlph)
-console.log(outputNumA)
-console.log(outputNumB)
 
 var ID = `${outputAlph}-${outputNumA}-${outputNumB}`
 
